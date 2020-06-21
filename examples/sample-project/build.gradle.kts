999f0852b15d1be3e4fc6b81fffc5215b562b0a2
PrzemyslawSwiderski <przemekzgierz7@gmail.com>
2020-06-21 21:27:38 +0200

    Revert "More informative exception message in case of missing secret."
    This reverts commit 85b76b32

--- examples/sample-project/build.gradle.kts
+++ examples/sample-project/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.pswidersk.yaml-secrets-plugin") version "1.0.7"
+    id("com.pswidersk.yaml-secrets-plugin") version "1.0.6"
 }
 val testProp = secrets.get<String>("test.testProp1")
 check(testProp == "test")
